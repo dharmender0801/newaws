17dd11fd2cc601c69da888d93a313cd52daaffa5
dharmender0801 <teamsoul234@gmail.com>
2023-10-06 12:05:19 +0530

    new adition

---
+++ Testing_CICD_pipeline/src/main/java/com/thread/TestingCicdPipelineApplication.java
@@ -0,0 +1,13 @@
+package com.thread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingCicdPipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestingCicdPipelineApplication.class, args);
+	}
+
+}
